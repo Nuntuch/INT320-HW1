796ecef03357d5d275fdf1f7b87c0adbd1145758
Nuntuch <nuntuch13589@gmail.com>
2019-02-08 14:37:47 +0700

    สร้างโครงต้นแบบ

---
+++ 60130500126-Sec4-Assignment/src/service/QueueGenericImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import service.base.QueueGeneric;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+public class QueueGenericImpl implements QueueGeneric{
+    
+}
