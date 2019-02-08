c934e63af50dd037d9111cf684aff6332c4bd4a0
Nuntuch <nuntuch13589@gmail.com>
2019-02-08 14:36:28 +0700

    สร้างโครงต้นแบบ

---
+++ 60130500126-Sec4-Assignment/src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+//import javax.inject.Inject;
+import service.base.QueueGeneric;
+import service.base.StackGeneric;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ */
+//@Inject
+
+public class Main implements QueueGeneric,StackGeneric{
+    public static void main(String[] args) {
+        
+    }
+}
