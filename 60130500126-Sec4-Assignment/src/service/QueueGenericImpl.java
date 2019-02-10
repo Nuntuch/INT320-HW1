408014c91cf534628686146b6611c7efe804b46b
Nuntuch <nuntuch13589@gmail.com>
2019-02-10 16:29:12 +0700

    implements all medthod

--- 60130500126-Sec4-Assignment/src/service/QueueGenericImpl.java
+++ 60130500126-Sec4-Assignment/src/service/QueueGenericImpl.java
@@ -12,5 +12,30 @@ import service.base.QueueGeneric;
  * @author Nuntuch Thongyoo
  */
 public class QueueGenericImpl implements QueueGeneric{
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getFront() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object dequeue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void enqueue(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
