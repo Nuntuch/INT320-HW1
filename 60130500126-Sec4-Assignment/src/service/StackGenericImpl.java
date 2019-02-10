c497b81dbf88c15bca44b8510625f03b6a09e5cb
Nuntuch <nuntuch13589@gmail.com>
2019-02-10 16:29:18 +0700

    implements all medthod

--- 60130500126-Sec4-Assignment/src/service/StackGenericImpl.java
+++ 60130500126-Sec4-Assignment/src/service/StackGenericImpl.java
@@ -12,5 +12,30 @@ import service.base.StackGeneric;
  * @author Nuntuch Thongyoo
  */
 public class StackGenericImpl implements StackGeneric{
+
+//    @Override
+//    public int OrderToEntryOutTheRoom() {
+//        throw new UnsupportedOperationException("Not supported yet.");
+//    }
+
+    @Override
+    public void push(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object peek() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
