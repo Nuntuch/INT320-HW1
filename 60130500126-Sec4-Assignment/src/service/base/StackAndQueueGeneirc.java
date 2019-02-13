214db71a626ad78407087e16739c8812b1ace880
Unknown <nuntuch13589@gmail.com>
2019-02-13 10:59:35 +0700

    สร้างโครงร่างที่สำคัญ

--- 60130500126-Sec4-Assignment/src/service/base/StackAndQueueGeneirc.java
+++ 60130500126-Sec4-Assignment/src/service/base/StackAndQueueGeneirc.java
@@ -5,10 +5,38 @@
  */
 package service.base;
 
+import java.util.Vector;
+
 /**
  *
  * @author Nuntuch Thongyoo
  */
 public interface StackAndQueueGeneirc {
-    
+
+    public interface QueueGeneric<Obj> {
+
+        public boolean isEmpty();
+
+        public Obj getFront();
+
+        public Obj dequeue();
+
+        public void enqueue(Obj obj);
+
+        public void clear();
+
+    }
+
+    public interface StackGeneric<Obj> {
+
+        public void push(Obj obj);
+
+        public Obj pop();
+
+        public Obj peek();
+
+        public boolean isEmpty();
+
+    }
+
 }
