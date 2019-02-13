cc07de6853d7dd1a197ef50ff3b694820db8737f
Unknown <nuntuch13589@gmail.com>
2019-02-13 13:25:02 +0700

    รวมเมตตอดที่ซ้ำเข้าด้วยกัน

--- 60130500126-Sec4-Assignment/src/service/base/StackAndQueueGeneirc.java
+++ 60130500126-Sec4-Assignment/src/service/base/StackAndQueueGeneirc.java
@@ -1,14 +1,17 @@
-
 package service.base;
+
 /**
  *
  * @author Nuntuch Thongyoo
  */
-public interface StackAndQueueGeneirc {
+
+public interface StackAndQueueGeneirc<Obj> {
+
+    public boolean isEmpty();
+
+    public int size();
 
     public interface QueueGeneric<Obj> {
-
-        public boolean isEmpty();
 
         public Obj getFront();
 
@@ -28,8 +31,36 @@ public interface StackAndQueueGeneirc {
 
         public Obj peek();
 
-        public boolean isEmpty();
-
     }
 
 }
+
+//public interface StackAndQueueGeneirc {
+//
+//    public interface QueueGeneric<Obj> {
+//
+//        public boolean isEmpty();
+//
+//        public Obj getFront();
+//
+//        public Obj dequeue();
+//
+//        public void enqueue(Obj obj);
+//
+//        public void clear();
+//
+//    }
+//
+//    public interface StackGeneric<Obj> {
+//
+//        public void push(Obj obj);
+//
+//        public Obj pop();
+//
+//        public Obj peek();
+//
+//        public boolean isEmpty();
+//
+//    }
+//
+//}
