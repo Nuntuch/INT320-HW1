189acf12f99abdfdc31f5e67b5dc5d68d10877b2
Unknown <nuntuch13589@gmail.com>
2019-02-14 09:44:36 +0700

    แก้บัคของclear และเขียน main ใหม่

---
+++ 60130500126-Sec4-Assignment/src/service/StackAndQueueImpl.java
@@ -0,0 +1,200 @@
+package service;
+
+import java.util.Arrays;
+import service.base.Queue;
+import service.base.Stack;
+
+/**
+ *
+ * @author Nuntuch Thongyoo
+ * @param <ObjG>
+ */
+public class StackAndQueueImpl<ObjG> implements Stack<ObjG>, Queue<ObjG> {
+
+    /////For Dev
+    public ObjG getValueByLocal(int value) {
+        return array[value];
+    }
+    /////   
+
+    public StackAndQueueImpl() {
+        this.numlocal = 0;
+    }
+
+//    private int numlocal = 0;
+    private int numlocal;
+//    private List ArrayList = new ArrayList();
+    private ObjG[] array = (ObjG[]) (new Object[10]);
+
+    @Override
+    public int size() {
+//        int i = numlocal+1;
+//        return i;
+        return numlocal;
+    }
+
+    @Override
+    public void push(ObjG obj) {
+
+//        if (this.numlocal == this.array.length) {
+        if (this.numlocal > this.array.length - 1) {
+//        if (this.numlocal%10 ==0) {
+
+            System.out.println("Expand Space is on!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            ObjG[] arraytemp = (ObjG[]) (new Object[numlocal + 10]);
+            for (int i = 0; i < numlocal; i++) {
+                arraytemp[i] = this.array[i];
+
+            }
+            this.array = (ObjG[]) (new Object[numlocal + 15]);
+
+            for (int i = 0; i < arraytemp.length; i++) {
+                this.array[i] = arraytemp[i];
+
+            }
+
+        }
+        System.out.println("Numlocal : " + numlocal+" Obj :  "+obj);
+        this.array[numlocal] = obj;
+        this.numlocal++;
+
+//        if (array.length < numlocal) {
+//            ObjG[] arraytemp = (ObjG[]) (new Object[numlocal + 15]);
+//            for (int i = 0; i < numlocal; i++) {
+//                arraytemp[i] = this.array[i];
+//
+//            }
+//
+//            for (int i = 0; i < arraytemp.length; i++) {
+//                this.array[i] = arraytemp[i];
+//
+//            }
+//
+//        }
+//
+//        this.array[numlocal] = obj;
+//        this.numlocal++;
+    }
+
+    @Override
+    public ObjG pop() {//ดึงตัวท้ายออก1ตัว
+        ObjG[] arraytemp = (ObjG[]) (new Object[numlocal - 2]);
+        for (int i = 0; i < numlocal - 2; i++) {
+            arraytemp[i] = this.array[i];
+
+        }
+
+        for (int i = 0; i < arraytemp.length; i++) {
+            this.array[i] = arraytemp[i];
+
+        }
+        this.numlocal--;
+//        return array[array.length - 1];
+        return array[numlocal];
+    }
+
+    @Override
+    public ObjG peek() {
+        return array[numlocal - 1];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numlocal == 0;
+    }
+
+    @Override
+    public ObjG getFront() {
+        return array[0];
+    }
+
+    @Override
+    public ObjG dequeue() {//เอาหน้าออก
+        ObjG[] arraytemp = (ObjG[]) (new Object[array.length]);
+        for (int i = 1; i < array.length - 2; i++) {
+            arraytemp[i - 1] = this.array[i];
+            System.out.println("this.array[" + i + "] : " + this.array[i - 1]);
+        }
+
+        for (int i = 0; i < arraytemp.length; i++) {
+            this.array[i] = arraytemp[i];
+
+        }
+        this.numlocal--;
+//        return array[array.length-1];
+        return array[0];
+    }
+
+    @Override
+    public void enqueue(ObjG obj) {
+
+        if (this.numlocal > this.array.length - 1) {
+//        if (this.numlocal%10 ==0) {
+
+            System.out.println("Expand Space is on!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            ObjG[] arraytemp = (ObjG[]) (new Object[numlocal + 10]);
+            for (int i = 0; i < numlocal; i++) {
+                arraytemp[i] = this.array[i];
+
+            }
+            this.array = (ObjG[]) (new Object[numlocal + 15]);
+
+            for (int i = 0; i < arraytemp.length; i++) {
+                this.array[i] = arraytemp[i];
+
+            }
+
+        }
+        System.out.println("Numlocal : " + numlocal+" Obj :  "+obj);
+//        System.out.println("Numlocal : " + numlocal);
+        this.array[numlocal] = obj;
+        this.numlocal++;
+
+    }
+
+    @Override
+    public void clear() {
+        array = (ObjG[]) (new Object[10]);
+        this.numlocal = 0;
+    }
+
+    @Override
+    public int search(ObjG obj) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (obj.equals(array[i])) {
+                return i;
+            }
+
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final StackAndQueueImpl<?> other = (StackAndQueueImpl<?>) obj;
+        if (!Arrays.deepEquals(this.array, other.array)) {
+            return false;
+        }
+        return true;
+    }
+
+}
--- 60130500126-Sec4-Assignment/src/service/base/StackAndQueueGeneirc.java
+++ 60130500126-Sec4-Assignment/src/service/base/StackAndQueueGeneirc.java
@@ -1,45 +1,17 @@
-package service.base;
-
-/**
- *
- * @author Nuntuch Thongyoo
- */
-
-public interface StackAndQueueGeneirc<Obj> {
-
-    public boolean isEmpty();
-
-    public int size();
-
-    public interface QueueGeneric<Obj> {
-
-        public Obj getFront();
-
-        public Obj dequeue();
-
-        public void enqueue(Obj obj);
-
-        public void clear();
-
-    }
-
-    public interface StackGeneric<Obj> {
-
-        public void push(Obj obj);
-
-        public Obj pop();
-
-        public Obj peek();
-
-    }
-
-}
-
-//public interface StackAndQueueGeneirc {
+//package service.base;
+//
+///**
+// *
+// * @author Nuntuch Thongyoo
+// */
+//
+//public interface StackAndQueueGeneirc<Obj> {
+//
+//    public boolean isEmpty();
+//
+//    public int size();
 //
 //    public interface QueueGeneric<Obj> {
-//
-//        public boolean isEmpty();
 //
 //        public Obj getFront();
 //
@@ -59,8 +31,36 @@ public interface StackAndQueueGeneirc<Obj> {
 //
 //        public Obj peek();
 //
-//        public boolean isEmpty();
-//
 //    }
 //
 //}
+//
+////public interface StackAndQueueGeneirc {
+////
+////    public interface QueueGeneric<Obj> {
+////
+////        public boolean isEmpty();
+////
+////        public Obj getFront();
+////
+////        public Obj dequeue();
+////
+////        public void enqueue(Obj obj);
+////
+////        public void clear();
+////
+////    }
+////
+////    public interface StackGeneric<Obj> {
+////
+////        public void push(Obj obj);
+////
+////        public Obj pop();
+////
+////        public Obj peek();
+////
+////        public boolean isEmpty();
+////
+////    }
+////
+////}
